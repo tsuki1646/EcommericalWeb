d5848bb724a3d8271d8a41de46a59a5a3e0ef718
tsuki1646 <aoanhtrang_16@yahoo.com>
2021-08-23 13:14:28 +0900

    Lesson 26: Trying login
    Lesson 26: Trying login

--- client/pages/login.js
+++ client/pages/login.js
@@ -1,8 +1,71 @@
+import { useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { SyncOutlined } from "@ant-design/icons";
+import Link from "next/link";
+
 const Login = () => {
+  const [email, setEmail] = useState("nmn.nguyet@gmail.com");
+  const [password, setPassword] = useState("123456");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // console.table({ name, email, password });
+    try {
+      setLoading(true);
+      const { data } = await axios.post(`/api/login`, {
+        email,
+        password,
+      });
+      console.log("LOGIN RESPONSE", data);
+      // setLoading(false);
+    } catch (err) {
+      toast(err.response.data);
+      setLoading(false);
+    }
+  };
+
   return (
     <>
-      <h1 className="p-5 mb-4 bg-light rounded-3 text-center square">Login</h1>
-      <p>Login page</p>
+      <h1 className="jumbotron text-center bg-primary square">Login</h1>
+
+      <div className="container col-md-4 offset-md-4 pb-5">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="email"
+            className="form-control mb-4 p-4"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter email"
+            required
+          />
+
+          <input
+            type="password"
+            className="form-control mb-4 p-4"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Enter password"
+            required
+          />
+
+          <button
+            type="submit"
+            className="btn btn-block btn-primary"
+            disabled={!email || !password || loading}
+          >
+            {loading ? <SyncOutlined spin /> : "Submit"}
+          </button>
+        </form>
+
+        <p className="text-center p-3">
+          Not yet registered?{" "}
+          <Link href="/register">
+            <a>Register</a>
+          </Link>
+        </p>
+      </div>
     </>
   );
 };
