efba25fd96a0c1278db25521de7e0d50aba76e37
tsuki1646 <aoanhtrang_16@yahoo.com>
2021-08-24 23:52:12 +0900

    Lesson 40:  Sending test email
    Lesson 40:  Sending test email

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,16 @@
 import User from "../models/user";
 import { hashPassword, comparePassword } from "../utils/auth";
 import jwt from "jsonwebtoken";
+import AWS from "aws-sdk";
+
+const awsConfig ={
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+  apiVersion: process.env.AWS_API_VERSION,
+}
+
+const SES = new AWS.SES(awsConfig);
 
 export const register = async (req, res) => {
   //res.json("register user response from controller!!");
@@ -83,41 +93,41 @@ export const currentUser = async (req, res) => {
 };
 
 export const sendTestEmail = async (req, res) => {
-   console.log("send email using SES");
-   res.json({ ok: true });
-  // const params = {
-  //   Source: process.env.EMAIL_FROM,
-  //   Destination: {
-  //     ToAddresses: ["nmn.nguyet@gmail.com"],
-  //   },
-  //   ReplyToAddresses: [process.env.EMAIL_FROM],
-  //   Message: {
-  //     Body: {
-  //       Html: {
-  //         Charset: "UTF-8",
-  //         Data: `
-  //           <html>
-  //             <h1>Reset password link</h1>
-  //             <p>Please use the following link to reset your password</p>
-  //           </html>
-  //         `,
-  //       },
-  //     },
-  //     Subject: {
-  //       Charset: "UTF-8",
-  //       Data: "Password reset link",
-  //     },
-  //   },
-  // };
+  //  console.log("send email using SES");
+  //  res.json({ ok: true });
+  const params = {
+    Source: process.env.EMAIL_FROM,
+    Destination: {
+      ToAddresses: ["nmn.nguyet@gmail.com"],
+    },
+    ReplyToAddresses: [process.env.EMAIL_FROM],
+    Message: {
+      Body: {
+        Html: {
+          Charset: "UTF-8",
+          Data: `
+            <html>
+              <h1>Reset password link</h1>
+              <p>Please use the following link to reset your password</p>
+            </html>
+          `,
+        },
+      },
+      Subject: {
+        Charset: "UTF-8",
+        Data: "Password reset link",
+      },
+    },
+  };
 
-  // const emailSent = SES.sendEmail(params).promise();
+  const emailSent = SES.sendEmail(params).promise();
 
-  // emailSent
-  //   .then((data) => {
-  //     console.log(data);
-  //     res.json({ ok: true });
-  //   })
-  //   .catch((err) => {
-  //     console.log(err);
-  //   });
+  emailSent
+    .then((data) => {
+      console.log(data);
+      res.json({ ok: true });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
