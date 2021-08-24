f79cb917e8445743cc383dc590dd19158d42abd5
tsuki1646 <aoanhtrang_16@yahoo.com>
2021-08-24 12:55:12 +0900

    Lesson 34: CSRF Protection
    Lesson 34: CSRF Protection

--- server/server.js
+++ server/server.js
@@ -2,16 +2,25 @@ import express from "express";
 import cors from "cors";
 import { readdirSync } from "fs";
 import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import csrf from "csurf";
+
 const morgan = require("morgan");
 require("dotenv").config({ path: "./config.env" });
+
+const csrfProtection = csrf({ cookie: true });
+
+//db
 const connectDb = require("./connectDb");
 connectDb();
+
 // create express app
 const app = express();
 
 // apply middlewares
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 app.use(morgan("dev"));
 app.use((req, res, next) => {
     console.log("this is my own middleware");
@@ -24,6 +33,13 @@ readdirSync("./routes").map(
         require(`./routes/${r}`)
     )
 );
+
+// csrf
+app.use(csrfProtection);
+
+app.get("/api/csrf-token", (req, res) => {
+  res.json({ csrfToken: req.csrfToken() });
+});
 
 // port
 const port = process.env.PORT || 8000;
