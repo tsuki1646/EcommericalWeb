761022e645ae1b590ff838d1fd976eda5138519f
tsuki1646 <aoanhtrang_16@yahoo.com>
2021-08-25 14:29:45 +0900

    Lesson 48: Conditional links & pages
    Lesson 48: Conditional links & pages

---
+++ client/pages/instructor/course/create.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const CourseCreate = () => {
+  return (
+    <>
+      <h1 className="p-5 mb-4 bg-light rounded-3 text-center square">Create Course</h1>
+    </>
+  );
+};
+
+export default createCourse;
---
+++ client/pages/user/become-instructor.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const BecomeInstructor = () => {
+  return (
+    <>
+      <h1 className="p-5 mb-4 bg-light rounded-3 text-center square">Become Instructor</h1>
+    </>
+  );
+};
+
+export default BecomeInstructor;
