6af2243b40da59efe77a8856f43852cbbf4af060
tsuki1646 <aoanhtrang_16@yahoo.com>
2021-08-25 20:59:20 +0900

    Lesson 62: Price dropdown & category
    Lesson 62: Price dropdown & category

--- client/components/forms/CourseCreateForm.js
+++ client/components/forms/CourseCreateForm.js
@@ -1,5 +1,4 @@
 import { Select, Button } from "antd";
-import { SaveOutlined } from "@ant-design/icons";
 
 const { Option } = Select;
 
@@ -9,79 +8,112 @@ const CourseCreateForm = ({
   handleChange,
   values,
   setValues,
-}) => (
-  <form onSubmit={handleSubmit}>
-    <div className="form-group mb-3 ">
-      <input
-        type="text"
-        name="name"
-        className="form-control"
-        placeholder="Name"
-        value={values.name}
-        onChange={handleChange}
-      />
-    </div>
+}) => {
+  const children = [];
+  for (let i = 9.99; i <= 100.99; i++) {
+    children.push(<Option key={i.toFixed(2)}>${i.toFixed(2)}</Option>);
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="form-group mb-3">
+        <input
+          type="text"
+          name="name"
+          className="form-control"
+          placeholder="Name"
+          value={values.name}
+          onChange={handleChange}
+        />
+      </div>
 
-    <div className="form-group mb-3">
-      <textarea
-        name="description"
-        cols="7"
-        rows="7"
-        value={values.description}
-        className="form-control"
-        onChange={handleChange}
-      ></textarea>
-    </div>
+      <div className="form-group mb-3">
+        <textarea
+          name="description"
+          cols="7"
+          rows="7"
+          value={values.description}
+          className="form-control"
+          onChange={handleChange}
+        ></textarea>
+      </div>
 
-    <div className="form-row mb-3">
-      <div className="col">
-        <div className="form-group">
-          <Select
-            style={{ width: "100%" }}
+      <div className="form-row mb-3">
+        <div className="col">
+            <div className="form-group mb-3">
+                <Select
+                style={{ width: "60%" }}
+                size="large"
+                value={values.paid}
+                onChange={(v) => setValues({ ...values, paid: !values.paid })}
+                >
+                    <Option value={true}>Paid</Option>
+                    <Option value={false}>Free</Option>
+                </Select>
+            </div>
+          
+        </div>
+
+        {values.paid && (
+            <div className="form-group mb-3">
+                <Select
+                defaultValue="$9.99"
+                style={{ width: "10%" }}
+                onChange={(v) => setValues({ ...values, price: v })}
+                tokenSeparators={[,]}
+                size="large"
+                >
+                {children}
+                </Select>
+            </div>
+        
+        )}
+      </div>
+
+      <div className="form-group mb-3">
+        <input
+          type="text"
+          name="category"
+          className="form-control"
+          placeholder="Category"
+          value={values.category}
+          onChange={handleChange}
+        />
+      </div>
+
+      <div className="form-row mb-3">
+        <div className="col">
+          <div className="form-group">
+            <label className="btn btn-outline-secondary btn-block text-left">
+              {values.loading ? "Uploading" : "Image Upload"}
+              <input
+                type="file"
+                name="image"
+                onChange={handleImage}
+                accept="image/*"
+                hidden
+              />
+            </label>
+          </div>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col">
+          <Button
+            onClick={handleSubmit}
+            disabled={values.loading || values.uploading}
+            className="btn btn-primary"
+            loading={values.loading}
+            type="primary"
             size="large"
-            value={values.paid}
-            onChange={(v) => setValues({ ...values, paid: !values.paid })}
+            shape="round"
           >
-            <Option value={true}>Paid</Option>
-            <Option value={false}>Free</Option>
-          </Select>
+            {values.loading ? "Saving..." : "Save & Continue"}
+          </Button>
         </div>
       </div>
-    </div>
-
-    <div className="form-row mb-3">
-      <div className="col">
-        <div className="form-group">
-          <label className="btn btn-outline-secondary btn-block text-left">
-            {values.loading ? "Uploading" : "Image Upload"}
-            <input
-              type="file"
-              name="image"
-              onChange={handleImage}
-              accept="image/*"
-              hidden
-            />
-          </label>
-        </div>
-      </div>
-    </div>
-
-    <div className="row">
-      <div className="col">
-        <Button
-          onClick={handleSubmit}
-          disabled={values.loading || values.uploading}
-          className="btn btn-primary"
-          loading={values.loading}
-          type="primary"
-          size="large"
-          shape="round"
-        >
-          {values.loading ? "Saving..." : "Save & Continue"}
-        </Button>
-      </div>
-    </div>
-  </form>
-);
+    </form>
+  );
+};
 
 export default CourseCreateForm;
