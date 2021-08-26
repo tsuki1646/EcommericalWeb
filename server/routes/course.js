79a428c50dbc3a57626c78b8df99786e86b0270f
tsuki1646 <aoanhtrang_16@yahoo.com>
2021-08-26 11:57:55 +0900

    Lesson 68: Delete image S3 server
    Lesson 68: Delete image S3 server

--- server/routes/course.js
+++ server/routes/course.js
@@ -6,8 +6,9 @@ const router = express.Router();
 import { requireSignin } from "../middlewares";
 
 // controllers
-import { uploadImage } from "../controllers/course";
+import { uploadImage, removeImage } from "../controllers/course";
 
 router.post("/course/upload-image", uploadImage);
+router.post("/course/remove-image", removeImage);
 
 module.exports = router;
