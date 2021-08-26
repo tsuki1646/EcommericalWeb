305ddd1d0286a421a80e5a64bf2f274f9ee547c2
tsuki1646 <aoanhtrang_16@yahoo.com>
2021-08-26 18:51:20 +0900

    Lesson 76: Display markdown context
    Lesson 76: Display markdown context

--- client/pages/instructor/course/view/[slug].js
+++ client/pages/instructor/course/view/[slug].js
@@ -1,9 +1,10 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import InstructorRoute from "../../../../components/routes/InstructorRoute";
 import axios from "axios";
 import { Avatar, Tooltip } from "antd";
 import { EditOutlined, CheckOutlined } from "@ant-design/icons";
+import ReactMarkdown from "react-markdown";
 
 const CourseView = () => {
     const [course, setCourse] = useState({});
@@ -57,6 +58,12 @@ const CourseView = () => {
                                     </Tooltip>
                                 </div>
                             </div>
+                        </div>
+                    </div>
+                    <hr />
+                    <div className="row">
+                        <div className="col">
+                            <ReactMarkdown children={course.description} />
                         </div>
                     </div>
                 </div>
